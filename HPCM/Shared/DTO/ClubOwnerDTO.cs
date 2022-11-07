8f4d8681d278b7220dce54aaf7dfbd1820eca5b9
daan.vander.haegen <daan.vander.haegen@student.howest.be>
2022-11-07 16:16:32 +0100

    finally able to get clubs from db after switching from azure data studio to dbeaver

---
+++ HPCM/Shared/DTO/ClubOwnerDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.DTO;
+
+public class ClubOwnerDTO
+{
+    public int MemberId { get; set; }
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+}
